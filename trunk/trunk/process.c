a3d4f2b90b6e9f82882563f9698c1eac15ad1391
dmanchon <dmanchon@d87b3b84-f980-11de-8cdf-e97ad7d0f28a>
2010-01-05 23:24:59 +0000

    example of how to write an AVI file

--- trunk/trunk/process.c
+++ trunk/trunk/process.c
@@ -2,60 +2,47 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-//opencv library
 #include "cv.h"
 #include "highgui.h"
 
-IplImage* doPyrDown( IplImage* in, int filter = IPL_GAUSSIAN_5x5)
-{
-	IplImage* out;
-	assert(in->width%2 == 0 && in->height%2 == 0);
+main( int argc, char* argv[] ) {
+	CvCapture* capture = cvCreateFileCapture( argv[1] );
+	IplImage *bgr_frame=cvQueryFrame(capture);//Init the video read
+	double fps = cvGetCaptureProperty (
+		capture,
+		CV_CAP_PROP_FPS
+		);
+	fprintf(stderr,"fps=%f\n",fps);
+	int x  = (int)cvGetCaptureProperty( capture, CV_CAP_PROP_FRAME_WIDTH);
+	fprintf(stderr,"x=%d\n",x);
+	int y =  (int)cvGetCaptureProperty( capture, CV_CAP_PROP_FRAME_HEIGHT);
+	fprintf(stderr,"y=%d\n",y);
 
-	out = cvCreateImage(cvSize(in->width/2,in->height/2),in->depth,in->nChannels);
-	cvPyrDown(in,out);
-	return out;
-}
-
-char example2_4(IplImage* img)
-{
-	IplImage* out = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,3);
-	char key;
-
-	cvShowImage("Example4-in",img);
-	//cvSmooth(img,out,CV_GAUSSIAN,5,5);
-	out = doPyrDown(img);
-	cvShowImage("Example4-out",out);
-	key = cvWaitKey(33);
-
-	cvReleaseImage(&out);
-	return key;
-}
-
-int main (int argc, char *argv[])
-{
-	IplImage* frame;
-	char key;
-	int frames;
-	CvCapture* g_capture;
-
-	cvNamedWindow("Example4-in");
-	cvNamedWindow("Example4-out");
-
-	fprintf(stderr,"Loading %s@%d...\n",argv[0],getpid());
-
-	g_capture = cvCreateFileCapture(argv[1]);
-
-	while(1)
-	{
-		frame = cvQueryFrame(g_capture);
-		if (!frame) break;
-		key = example2_4(frame);
-		if (key == 27) break;
+	CvSize size = cvSize(
+		x,
+		y		
+		);
+	CvVideoWriter *writer = cvCreateVideoWriter(
+		argv[2],
+		CV_FOURCC('D','I','V','X'),
+		fps,
+		size
+		);
+	IplImage* logpolar_frame = cvCreateImage(
+		size,
+		IPL_DEPTH_8U,
+		3
+		);
+	while( (bgr_frame=cvQueryFrame(capture)) != NULL ) {
+		cvLogPolar( bgr_frame, logpolar_frame,
+			    cvPoint2D32f(bgr_frame->width/2,
+					 bgr_frame->height/2),
+			    40,
+			    CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS );
+		cvWriteFrame( writer, logpolar_frame );
 	}
-
-
-	cvReleaseCapture(&g_capture);
-	cvDestroyWindow("Example4-in");
-	cvDestroyWindow("Example4-out");
-
+	cvReleaseVideoWriter( &writer );
+	cvReleaseImage( &logpolar_frame );
+	cvReleaseCapture( &capture );
+	return(0);
 }
