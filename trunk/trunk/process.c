ed88fb234dbdf5704bb4de6a48eba851b1b13d72
dmanchon <dmanchon@d87b3b84-f980-11de-8cdf-e97ad7d0f28a>
2010-01-05 20:03:50 +0000

    gui example, its not working in linux due some library problems

--- trunk/trunk/process.c
+++ trunk/trunk/process.c
@@ -8,19 +8,38 @@
 
 
 
+int g_slider_position = 0;
+CvCapture* g_capture = NULL;
+
+void onTrackbarSlide( int pos )
+{
+	cvSetCaptureProperty(g_capture,CV_CAP_PROP_POS_FRAMES,pos);
+}
+
 int main (int argc, char *argv[])
 {
 	IplImage* frame;
-	CvCapture* capture = cvCreateFileCapture(argv[1]);
 	char key;
+	int frames;
 
 	fprintf(stderr,"Loading %s@%d...\n",argv[0],getpid());
 
 	cvNamedWindow("win",CV_WINDOW_AUTOSIZE);
+	g_capture = cvCreateFileCapture(argv[1]);
+
+	//attention: this is not working in linux, always return 0
+	frames = (int) cvGetCaptureProperty(g_capture,CV_CAP_PROP_FRAME_COUNT);
+	fprintf(stderr,"Number of frames: %d\n",frames);
+
+	frames = 100;
+	if (frames != 0)
+	{
+		cvCreateTrackbar("Position","win",&g_slider_position,frames,onTrackbarSlide);
+	}
 
 	while(1)
 	{
-		frame = cvQueryFrame(capture);
+		frame = cvQueryFrame(g_capture);
 		if (!frame) break;
 		cvShowImage("win",frame);
 		key = cvWaitKey(33);
@@ -28,6 +47,6 @@ int main (int argc, char *argv[])
 	}
 
 
-	cvReleaseCapture(&capture);
+	cvReleaseCapture(&g_capture);
 	cvDestroyWindow("win");
 }
