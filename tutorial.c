50e0d25b789f1a3320db9239c94b140f03eb7f5c
dmanchon <dmanchon@d87b3b84-f980-11de-8cdf-e97ad7d0f28a>
2010-01-09 17:59:46 +0000

    opencv tutorial notes

---
+++ tutorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//opencv library
+#include "cv.h"
+#include "cxtypes.h"
+#include "highgui.h"
+
+
+int main (int argc, char *argv[])
+{
+	fprintf(stderr,"Loading %s@%d...\n",argv[0],getpid());
+
+
+
+
+}
